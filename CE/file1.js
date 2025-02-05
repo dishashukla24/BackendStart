6bdc6ec5411da1746b0f41d7430ae1e518ea2d25
Disha Shukla <dishashukla0110@gmail.com>
2025-02-05 09:45:30 +0530

    Work

--- CE/file1.js
+++ CE/file1.js
@@ -1,3 +1,6 @@
 const fs = require('fs');  //fs: file system
-const data = fs.readFileSync("./data.txt","utf-8");
-console.log(data);  //data.toString()
+const data = fs.readFileSync("./data.txt","utf-8"); //{encoding: "utf-8"}
+//console.log(data);  //data.toString()
+fs.writeFileSync("./data1.txt","CE promise");
+fs.appendFileSync("./data1.txt","AKTU");
+fs.renameSync("./data1.txt","./data2.txt")
