08c7b417c4d2953b47b93546cbce396cdab6dc7b
Disha Shukla <dishashukla0110@gmail.com>
2025-02-19 10:21:47 +0530

    api work

---
+++ CE/HTTP/http-gitapi.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const axios = require('axios')
+const port = 8000;
+const server = http.createServer(async(req,res) => {
+    console.log('New request received');
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+   const response =  await axios.get("https://api.github.com/search/users",{
+    params: {
+        q: "location:ghaziabad"
+    }
+   });
+   const usersdata =  response.data.items;
+   let frontdata = `<html><head></head><body>` 
+   usersdata.forEach((users) => {
+    frontdata += `<div><img src="${users.avatar_url}" /></div>`;
+
+   });
+   frontdata += `</body></html>`
+    res.end(frontdata);
+});
+server.listen (port, () => {
+    console.log(`Server is running on the port ${port}`);
+})
